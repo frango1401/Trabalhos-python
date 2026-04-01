a72bac79ccdb4e99d5a3f3eeab613e06a027b720
frango1401 <Franciscodemarchi8@gmail.com>
2026-04-01 12:37:45 -0300

    Add files via upload

---
+++ apro repro media.py
@@ -0,0 +1,11 @@
+a = int(input("digite a primeira nota: "))
+b = int(input("digite a segundo nota: "))
+
+c = (a + b) / 2
+
+if c >= 70:
+    print(f"aprovado com {c} de nota.")
+else:
+    print(f"reprovado sua media é {c}.")
+
+#Francisco Zanotelli Demarchi
